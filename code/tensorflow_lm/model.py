da7ae433213b601e3b3470de0f75a178479a797f
Harsh Jhamtani <harshjhamtani@gmail.com>
2017-05-10 10:42:32 -0400

    [Code] edits to LM model

--- code/tensorflow_lm/model.py
+++ code/tensorflow_lm/model.py
@@ -12,55 +12,43 @@ class RNNModel:
 		print "========== INIT ============= "
 		self.use_reverse_encoder = False
 		if mode=='training':
-			self.token_lookup_sequences_decoder_placeholder_list = []
+			self.token_input_sequences_placeholder_list = []
 			self.masker_list = []
-			self.token_output_sequences_decoder_placeholder_list = []
-			self.token_lookup_sequences_placeholder_list = []
+			self.token_output_sequences_placeholder_list = []
 
 			for bucket_num, bucket in buckets_dict.items():
-				max_sentence_length = bucket['max_input_seq_length']
-				self.token_lookup_sequences_placeholder_list.append( tf.placeholder("int32", [None, max_sentence_length], name="token_lookup_sequences"+str(bucket_num))  )# token_lookup_sequences
-				
-				max_sentence_length = bucket['max_output_seq_length']
-				self.masker_list.append( tf.placeholder("float32", [None, max_sentence_length], name="masker"+str(bucket_num)) )
-				self.token_output_sequences_decoder_placeholder_list.append( tf.placeholder("int32", [None, max_sentence_length], name="token_output_sequences_decoder_placeholder"+str(bucket_num)) )
-				self.token_lookup_sequences_decoder_placeholder_list.append( tf.placeholder("int32", [None, max_sentence_length], name="token_lookup_sequences_decoder_placeholder"+str(bucket_num)) ) # token_lookup_sequences
+				max_input_seq_length = bucket['max_input_seq_length']
+				self.token_input_sequences_placeholder_list.append( tf.placeholder("int32", [None, max_input_seq_length], name="token_input_sequences"+str(bucket_num))  )# token_lookup_sequences
+				self.masker_list.append( tf.placeholder("float32", [None, max_input_seq_length], name="masker"+str(bucket_num)) )
+				self.token_output_sequences_placeholder_list.append( tf.placeholder("int32", [None, max_input_seq_length], name="token_output_sequences_placeholder"+str(bucket_num)) )
 		print "========== INIT OVER ============= "
 
 
-	def _getEncoderInitialState(self,cell, batch_size):
+	def _getInitialState(self,cell, batch_size):
 		return cell.zero_state(batch_size, tf.float32)
 
-	def runDecoderStep(self, lstm_cell, cur_inputs, prev_cell_output, state, encoder_outputs, reuse=False):
-		print "cur_inputs = ",cur_inputs
-		inputs = tf.concat([ cur_inputs, context ], axis=1)
-		print "inputs = ",inputs
-		print "conterxt = ",context
-		return lstm_cell(inputs, state=state), context, alpha
+	def _runDecoderStep(self, lstm_cell, cur_inputs, state, reuse=False):
+		inputs = cur_inputs
+		return lstm_cell(inputs, state=state)
 
-
-	def initDecoderOutputVariables(self,lstm_cell_size, token_vocab_size):
+	def _initDecoderOutputVariables(self,lstm_cell_size, token_vocab_size):
 		with tf.variable_scope('decoder_output', reuse=None) as scope:
 			w_out = tf.get_variable('w_out', shape=[lstm_cell_size, token_vocab_size], initializer=tf.random_normal_initializer(-1.0,1.0))
 			w_out = tf.get_variable('b_out', shape=[token_vocab_size]) # , initializer=tf.random_normal())
-			w_context_out = tf.get_variable('w_context_out', shape=[lstm_cell_size, token_vocab_size], initializer=tf.random_normal_initializer(-1.0,1.0))
-			b_context_out = tf.get_variable('b_context_out', initializer=tf.random_normal([token_vocab_size]) )
 			scope.reuse_variables()
 
-	def getDecoderOutputVariables(self):
+	def _getDecoderOutputVariables(self):
 		with tf.variable_scope('decoder_output', reuse=True) as scope:
 			w_out = tf.get_variable('w_out')
 			b_out = tf.get_variable('b_out')
-			w_context_out = tf.get_variable('w_context_out')
-			b_context_out = tf.get_variable('b_context_out')
-			return w_out, b_out, w_context_out, b_context_out
+			return w_out, b_out
 
-	def getDecoderOutput(self, output, lstm_cell_size, token_vocab_size, w_out, b_out, w_context_out, b_context_out, context=None ): # outputs_list: list of tensor(batch_size, cell_size) with time_steps number of items
+	def _getDecoderOutput(self, output, lstm_cell_size, token_vocab_size, w_out, b_out ): # outputs_list: list of tensor(batch_size, cell_size) with time_steps number of items
 		pred = tf.matmul(output, w_out) #+ b_out  #(N,vocab_size)
-		pred += (tf.matmul(context, w_context_out) + b_context_out)  #(N,vocab_size)
 		return pred
 
-	def initEmbeddings(self, emb_scope, token_vocab_size, embeddings_dim, reuse=False, pretrained_embeddings=None):
+	def _initEmbeddings(self, emb_scope, token_vocab_size, embeddings_dim, reuse=False, pretrained_embeddings=None):
+		'''
 		with tf.variable_scope(emb_scope):
 			if pretrained_embeddings!=None:
 				token_emb_mat = tf.get_variable("emb_mat", shape=[token_vocab_size, embeddings_dim], dtype='float', initializer=tf.constant_initializer(np.array(pretrained_embeddings)) )
@@ -71,18 +59,18 @@ class RNNModel:
 				token_emb_mat = tf.concat( [tf.zeros([1, embeddings_dim]), tf.slice(token_emb_mat, [1,0],[-1,-1]) ], axis=0 )	
 				#print "token_emb_mat = ",token_emb_mat
 		return token_emb_mat
+		'''
+		print "UNIMPLEMENTED...."
 
-	def greedyInferenceModel(self, params ):
+	def _greedyInferenceModel(self, params ):
 		lstm_cell = params['lstm_cell']
-		encoder_outputs = params['encoder_outputs']
 		token_vocab_size = params['vocab_size']
 		lstm_cell_size = params['lstm_cell_size']
 		batch_size = params['batch_size']
 		embeddings_dim = params['embeddings_dim']
-		batch_time_steps = params['max_output_seq_length']
+		batch_time_steps = params['max_inp_seq_length']
 		token_emb_mat = params['token_emb_mat']
-		w_out, b_out, w_context_out, b_context_out = params['output_vars']
-		encoder_outputs = params['encoder_outputs']
+		w_out, b_out = params['output_vars']
 		cell_output, state = params['cell_state']
 
 		num_steps = batch_time_steps
@@ -94,9 +82,9 @@ class RNNModel:
 				#outputs.append( tf.reshape(inp,[batch_size]) )
 			inputs_current_time_step = tf.reshape( tf.nn.embedding_lookup(token_emb_mat, inp) , [-1, embeddings_dim] )
 			if time_step > 0: tf.get_variable_scope().reuse_variables()
-			(cell_output, state), context, alpha = self.runDecoderStep(lstm_cell=lstm_cell, cur_inputs=inputs_current_time_step, encoder_outputs=encoder_outputs, prev_cell_output=cell_output, reuse=(time_step!=0), state=state)
+			(cell_output, state) = self._runDecoderStep(lstm_cell=lstm_cell, cur_inputs=inputs_current_time_step, reuse=(time_step!=0), state=state)
 			# cell_output: (N,cell_size)
-			cur_outputs = self.getDecoderOutput(cell_output, lstm_cell_size, token_vocab_size, w_out, b_out, w_context_out, b_context_out, context)
+			cur_outputs = self._getDecoderOutput(cell_output, lstm_cell_size, token_vocab_size, w_out, b_out)
 			assert cur_outputs.shape[1]==token_vocab_size
 			word_predictions = tf.argmax(cur_outputs,axis=1)
 			outputs.append(word_predictions)
@@ -104,15 +92,14 @@ class RNNModel:
 		return outputs
 
 
-	def decoderRNN(self, x, params, reuse=False, mode='training'):
+	def _decoderRNN(self, x, params, reuse=False, mode='training'):
 
 		lstm_cell = params['lstm_cell']
-		encoder_outputs = params['encoder_outputs']
 		token_vocab_size = params['vocab_size']
 		lstm_cell_size = params['lstm_cell_size']
 		batch_size = params['batch_size']
 		embeddings_dim = params['embeddings_dim']
-		batch_time_steps = params['max_output_seq_length']
+		batch_time_steps = params['max_inp_seq_length']
 		if 'token_emb_mat' in params:
 			token_emb_mat = params['token_emb_mat']
 		else:
@@ -127,18 +114,18 @@ class RNNModel:
 			num_steps = x.shape[1]
 		inputs = x
 
-		self.decoder_cell = cell = lstm_cell
+		self.lstm_cell = cell = lstm_cell
 
 		# inital state
-		decoder_initial_state = self.getInitialState(encoder_outputs, lstm_cell_size, reuse=reuse)
+		initial_state = self._getInitialState(cell, lstm_cell_size, reuse=reuse)
 
 		#decoder output variable
-		self.initDecoderOutputVariables(lstm_cell_size,token_vocab_size)
-		w_out, b_out, w_context_out, b_context_out = self.getDecoderOutputVariables()
+		self._initDecoderOutputVariables(lstm_cell_size, token_vocab_size)
+		w_out, b_out = self._getDecoderOutputVariables()
 
 		#unrolled lstm 
 		outputs = [] # h values at each time step
-		state = decoder_initial_state
+		state = initial_state
 		cell_output = state[1]
 		encoder_outputs = tf.stack(encoder_outputs) # timesteps, N, cellsize
 		encoder_outputs = tf.transpose(encoder_outputs,[1,0,2]) # N, timesteps, cellsize 
@@ -150,17 +137,14 @@ class RNNModel:
 					inputs_current_time_step = inputs[:, time_step, :]
 					#print "inputs_current_time_step.shape: ",inputs_current_time_step.shape
 					print "state = ",state
-					(cell_output, state), context, alpha = self.runDecoderStep(lstm_cell=lstm_cell, cur_inputs=inputs_current_time_step, encoder_outputs=encoder_outputs, prev_cell_output=cell_output, reuse=(time_step!=0), state=state)
+					(cell_output, state) = self._runDecoderStep(lstm_cell=lstm_cell, cur_inputs=inputs_current_time_step, reuse=(time_step!=0), state=state)
 					#print(cell_output.shape)
 					outputs.append(cell_output)
-					cur_pred = self.getDecoderOutput(cell_output, lstm_cell_size, token_vocab_size, w_out, b_out, w_context_out, b_context_out, context)
+					cur_pred = self._getDecoderOutput(cell_output, lstm_cell_size, token_vocab_size, w_out, b_out)
 					pred.append(cur_pred)
 				pred = tf.stack(pred)
 				outputs_tensor = tf.stack(outputs) 
 				outputs = tf.unstack(outputs_tensor)
-				#pred = self.getDecoderOutput(outputs, lstm_cell_size, token_vocab_size, w_out, b_out, w_context_out, b_context_out, context)
-				#print "--->>>>>> ",type(pred)
-				#print pred[0].shape
 				tf.get_variable_scope().reuse_variables()
 
 			elif mode=='inference':
@@ -171,10 +155,8 @@ class RNNModel:
 				params['encoder_outputs'] = encoder_outputs
 				params['cell_state'] = cell_output, state
 				params['beam_size'] = 20
-				outputs =  self.greedyInferenceModel(params) #self.beamSearchInference(params)  #self.greedyInferenceModel(params)
-				ret_encoder_outputs = tf.transpose(encoder_outputs,[1,0,2]) # N, timesteps, cellsize 
-				pred = outputs, ret_encoder_outputs
-
+				outputs =  self._greedyInferenceModel(params) #self.beamSearchInference(params)  #self.greedyInferenceModel(params)
+				pred = outputs
 				#Beam search
 		return pred
 
@@ -182,7 +164,7 @@ class RNNModel:
 	#################################################################################################################
 
 
-	def getDecoderModel(self, config, encoder_outputs, is_training=False, mode='training', reuse=False, bucket_num=0 ):
+	def getDecoderModel(self, config, is_training=False, mode='training', reuse=False, bucket_num=0 ):
 
 		print "==========================================================="
 		if mode=='inference' and is_training:
@@ -191,21 +173,19 @@ class RNNModel:
 		print " IN DECODER MODEL :: ",encoder_outputs[0].shape
 
 		token_vocab_size = config['vocab_size']
-		max_sentence_length = config['max_output_seq_length']
+		max_sentence_length = config['max_inp_seq_length']
 		embeddings_dim = config['embeddings_dim']
 		lstm_cell_size = config['lstm_cell_size']
 
 		#placeholders
 		if mode=='training':
-			token_lookup_sequences_decoder_placeholder =self.token_lookup_sequences_decoder_placeholder_list[bucket_num]
+			token_input_sequences_placeholder = self.token_input_sequences_placeholder_list[bucket_num]
 			masker = self.masker_list[bucket_num]
-			token_output_sequences_placeholder = self.token_output_sequences_decoder_placeholder_list[bucket_num]
+			token_output_sequences_placeholder = self.token_output_sequences_placeholder_list[bucket_num]
 
 		#embeddings
 		share_embeddings=False
-		emb_scope = 'emb_decoder'
-		if share_embeddings:
-			emb_scope='emb'
+		emb_scope='emb'
 		if reuse:
 			token_emb_mat = self.decoder_token_emb_mat
 		else:
@@ -222,17 +202,14 @@ class RNNModel:
 			if mode=='inference':
 				params={k:v for k,v in config.items()}
 				params['lstm_cell'] = lstm_cell 
-				params['encoder_outputs'] = encoder_outputs
 				params['token_emb_mat'] = token_emb_mat
 				inp= None #tf.nn.embedding_lookup(token_emb_mat, token_lookup_sequences_decoder_placeholder)
-				pred = self.decoderRNN(inp, params, mode='inference')
+				pred = self._decoderRNN(inp, params, mode='inference')
 			elif mode=='training':
 				params={k:v for k,v in config.items()}
 				params['lstm_cell'] = lstm_cell 
-				params['encoder_outputs'] = encoder_outputs
-				#params['token_emb_mat'] = None
-				inp = tf.nn.embedding_lookup(token_emb_mat, token_lookup_sequences_decoder_placeholder) 
-				pred = self.decoderRNN(inp, params, mode='training')  # timesteps, N, vocab_size
+				inp = tf.nn.embedding_lookup(token_emb_mat, token_output_sequences_placeholder) 
+				pred = self._decoderRNN(inp, params, mode='training')  # timesteps, N, vocab_size
 				pred_for_loss = pred # since sparse_softmax_cross_entropy_with_logits takes softmax on its own as well
 				pred = tf.unstack(pred)
 				pred = tf.stack( [ tf.nn.softmax(vals) for vals in pred ] )
