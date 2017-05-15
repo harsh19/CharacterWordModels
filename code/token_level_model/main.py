13fe468a207396bf3798a38bb7c985871dabd7a8
vgtomahawk <vgtomahawk@gmail.com>
2017-05-15 10:54:14 -0400

    [code] [token level model] placeholders

---
+++ code/token_level_model/main.py
@@ -0,0 +1,294 @@
+import matplotlib.pyplot as plt
+from nltk import word_tokenize
+import numpy as np
+import csv
+import configuration as config
+from sklearn.preprocessing import LabelEncoder
+import pickle
+import utilities as datasets
+import utilities
+import solver
+from keras.callbacks import ModelCheckpoint
+import tensorflow as tf
+
+# Set seed for reproducability
+tf.set_random_seed(1)
+
+
+debug_mode = True
+all_lengths = []
+
+class PreProcessing:
+
+	def __init__(self, params):
+		self.unknown_word = "UNKNOWN" # not used
+		self.sent_start = "SENTSTART".lower()
+		self.sent_end = "SENTEND".lower()
+		self.pad_word = "PADWORD".lower()
+		self.word_index = {}
+		self.index_word = {} # populated at end of all the processing
+		self.word_index_counter = 0
+
+		self.word_index[self.pad_word] = self.word_index_counter
+		self.word_index_counter+=1
+		self.word_index[self.sent_start] = self.word_index_counter
+		self.word_index_counter+=1
+		self.word_index[self.sent_end] = self.word_index_counter
+		self.word_index_counter+=1
+
+		self.params = params
+
+	def pad_sequences_my(self, sequences, maxlen, padding='post', truncating='post'):
+		ret=[]
+		# truncating 'pre' not supported as of now
+		pad_word_idx = self.word_index[self.pad_word]
+		for sequence in sequences:
+			if len(sequence)>=maxlen:
+				sequence=sequence[:maxlen]
+			else:
+				if padding=='post':
+					sequence = sequence + [pad_word_idx]*(maxlen - len(sequence))
+				else:
+					sequence = [pad_word_idx]*(maxlen - len(sequence)) + sequence
+			ret.append(sequence)
+		return np.array(ret)
+
+	
+	def fromIdxSeqToVocabSeq(self, seq):
+		return [self.index_word[x] for x in seq]
+
+	def tokenizer(self, texts):
+		global all_lengths
+		ret = [ word_tokenize(text.lower()) for text in texts ]
+		for text in ret:
+			for token in text:
+				if token not in self.word_index:
+					self.word_index[token] = self.word_index_counter
+					self.word_index_counter+=1
+			all_lengths.append(len(text))
+		ret = [ [ self.word_index[t] for t in text ] for text in ret ]
+		return ret
+
+	def char_tokenizer(self, texts, special_tokens={'<unk>':'U'}):
+		global all_lengths
+		print texts[7]
+		for k,v in special_tokens.items():
+			texts = [  text.replace(k,v) for text in texts ]
+		print texts[7]
+		for text in texts:
+			for ch in text:
+				if ch not in self.word_index:
+					self.word_index[ch] = self.word_index_counter
+					self.word_index_counter+=1
+			all_lengths.append(len(text))
+		ret = [ [ self.word_index[t] for t in text ] for text in texts ]
+		return ret
+
+	def loadData(self, split='train', char_or_word="word"):   
+		print "-----------------loadData()--------- split= ",split
+		data_src = config.data_src + "." + split + ".txt"
+		texts=[]
+		if debug_mode:
+			with open(data_src,"r") as fr:
+				ctr=0
+				for line in fr:
+					texts.append(line)
+					ctr+=1
+					if ctr>200:
+						break
+		else:
+			texts = open(data_src,"r").readlines()
+		if char_or_word=="word":
+			texts = [self.sent_start + " " + text + " " + self.sent_end for text in texts]
+			sequences = self.tokenizer(texts)
+			word_index = self.word_index
+			#TO DO: add bucketing...
+		else: # character
+			sequences = self.char_tokenizer(texts)
+			word_index = self.word_index			
+			sequences = [ [self.word_index[self.sent_start]] + sequence + [self.word_index[self.sent_end]] for sequence in sequences]
+			#TO DO: add bucketing...
+		sequences = self.pad_sequences_my(sequences, maxlen=config.MAX_SEQUENCE_LENGTH)	
+		#word_index[self.unknown_word]=0
+		print sequences[0]
+		print texts[0]
+		print "-----------------Done loadData()---------"
+		return sequences
+
+        def normalizeAndExpVals(self, vals):
+            # e^x[i] / sum(e^x[j])
+            maxval = np.max(vals)
+            print "maxval = ",maxval
+            vals = vals - maxval
+            vals = np.exp(vals)
+            return vals/sum(vals)
+
+	def loadTeacherProbValuesForTrain(self, src=config.dumped_train_probs_path):
+		data = open(src,"r").readlines()
+		ret = [ float(row.strip().split('\t')[1]) for row in data ]
+		ret = np.array(ret)
+		# change from log prob to prob
+		## ret = np.exp(ret) # chance of underflow ??
+                #Added By Varun:Start
+                ret=np.exp(ret)
+                Z=np.sum(ret)
+                ret=ret/Z
+                #Added By Varun:End
+                #ret = self.normalizeAndExpVals(ret)
+                #print "First 10 values after normalization : ",ret[:10]
+		return ret
+
+	def prepareLMdata(self, sequences):
+		data = np.array( [ sequence[:-1] for sequence in sequences ] )
+		if self.params['use_tf']:
+			labels = np.array( [sequence[1:] for sequence in sequences ] )
+		else:
+			labels = np.array( [ np.expand_dims(sequence[1:],-1) for sequence in sequences ] )
+
+		if debug_mode:
+			data = data[:192]
+			labels = labels[:192]
+		else:
+			batch_sz = config.batch_size
+			lim = batch_sz * (  len(data)/batch_sz ) # TO DO: Add support for incomplete batch in the model or in solver
+			data = data[:lim]
+			labels = labels[:lim]
+		return data,labels
+
+def getPretrainedEmbeddings(src):
+	ret={}
+	vals = open(src,"r").readlines()
+	vals = [val.strip().split('\t') for val in vals]
+	for val in vals:
+		#print val[0]
+		ret[val[0]] = [float(v) for v in val[1:]]
+		assert len( ret[val[0]] ) == 50
+	ret['sentstart'] = ret['SENT_START']
+	ret['sentend'] = ret['SENT_END']
+	return ret
+
+
+def main():
+
+	# buckets
+	buckets = {  0:{'max_input_seq_length': config.MAX_SEQUENCE_LENGTH-1 } } # 40-1=39
+	print buckets
+	print "==================================================="
+
+	# get model
+	params = {}
+	params['embeddings_dim'] =  config.embeddings_dim
+	params['lstm_cell_size'] = config.lstm_cell_size
+	params['max_input_seq_length'] = config.MAX_SEQUENCE_LENGTH - 1 #inputs are all but last element, outputs are al but first element
+	params['batch_size'] = config.batch_size
+	params['pretrained_embeddings']=False
+	params['char_or_word'] = config.char_or_word
+	params['use_tf'] = True
+	params['keep_prob'] = 1.0 - config.dropout_val
+	print params
+
+	preprocessing = PreProcessing(params)
+
+	train_sequences = preprocessing.loadData(split='train', char_or_word=params['char_or_word'] )		
+	val_sequences = preprocessing.loadData(split='valid', char_or_word=params['char_or_word'] )
+	test_sequences = preprocessing.loadData(split='test', char_or_word=params['char_or_word'] )	
+	print preprocessing.word_index[preprocessing.sent_start]
+	print preprocessing.word_index[preprocessing.sent_end]
+	index_word = {i:w for w,i in preprocessing.word_index.items()}
+	preprocessing.index_word = index_word
+	train = preprocessing.prepareLMdata(train_sequences)
+	val = preprocessing.prepareLMdata(val_sequences)
+	test = preprocessing.prepareLMdata(test_sequences)
+	train_weights = preprocessing.loadTeacherProbValuesForTrain() # the function will pick the default path from config
+	
+	#Sanity check
+	trainx, trainy = train
+	print train_weights.shape
+        print trainx.shape
+        if not debug_mode:
+                assert train_weights.shape[0] == trainx.shape[0]
+
+	# seq. length analysis
+	global all_lengths
+	all_lengths = np.array(all_lengths)
+	import scipy.stats
+	print " *** ", scipy.stats.describe(all_lengths)
+
+	params['vocab_size'] =  preprocessing.word_index_counter
+	print "params['vocab_size'] --------> ",params['vocab_size']
+
+	data,labels = train
+	print "--------- SAMPLE & shape..."
+	print "train data.shape ",data.shape
+	print "train labels.shape ",labels.shape
+	print "sameple:", data[0]
+	print "sample outputs: ",labels[0]
+	print "-------------------"
+	
+	if params['use_tf']:
+		# model
+		mode=  ["inference", "train"][1]
+		print "mode = ",mode
+		if mode=='train':
+			train_buckets = {}
+			train = data, labels, train_weights
+			for bucket,_ in enumerate(buckets):
+				train_buckets[bucket] = train
+
+			rnn_model = solver.Solver(buckets)
+			_ = rnn_model.getModel(params, mode='train',reuse=False, buckets=buckets)
+			rnn_model.trainModel(config=params, train_feed_dict=train_buckets, val_feed_dct=val, reverse_vocab=preprocessing.index_word, do_init=True)
+		
+		else:
+			rnn_model = solver.Solver(buckets=None, mode='inference')
+			params['max_inp_seq_length'] = 39
+			params['saved_model_path'] = config.saved_model_path
+			_ = rnn_model.getModel(params, mode='inference', reuse=False, buckets=None)
+			data_typ=["train","val"][1]
+			if data_typ=="val":
+				valx, valy = val
+			else:
+				valx, valy = train
+			dump_seq_prob=True
+			dump_seq_prob_path="./tmp/" + data_typ + "_groundtruth_probs.txt"
+			rnn_model.solveAll(params, valx, valy, preprocessing.index_word, sess=None, dump_seq_prob=dump_seq_prob, dump_seq_prob_path=dump_seq_prob_path)
+	else: #Keras
+		print "KERAS MODEL"
+
+		params['inp_length'] = 	params['max_input_seq_length']
+		rnn_model = keras_models.RNNModel()
+		model = rnn_model.getModel(params)
+		
+		#x_train, y_train, x_val, y_val, x_test, y_test = preprocessing.x_train, preprocessing.y_train, preprocessing.x_val, preprocessing.y_val, preprocessing.x_test, preprocessing.y_test
+		x_train, y_train = train
+		x_val, y_val = val
+		x_test, y_test = test
+
+		#call backs
+		perplexity_callback = keras_models.PerplexityCalculator()
+		weight_save_callback = ModelCheckpoint( 'tmp/keras_checkpoints/' + '_weights.{epoch:02d}-{val_loss:.2f}.hdf5', monitor='val_loss',verbose=0, save_best_only=False, mode='auto')
+
+		# train
+		keras_models.validation_data = (x_val, y_val)
+		model.fit(x_train, y_train, validation_data=(x_val, y_val),
+				  nb_epoch=3, batch_size= params['batch_size'], callbacks=[weight_save_callback, perplexity_callback] ) #config.num_epochs
+		#saveEmbeddings(model, preprocessing.word_index)
+				  
+		#evaluate
+		scores = model.evaluate(x_test, y_test, verbose=0)
+		print("Accuracy: %.2f%%" % (scores[1]*100))
+
+		print "--- Sampling few sequences.. "
+		for i in range(5):
+			print ""
+			pred = utilities.generateSentence(model, preprocessing.word_index, preprocessing.sent_start, 
+				preprocessing.sent_end, preprocessing.unknown_word)
+			sent = [preprocessing.index_word[i] for i in pred]
+			if params['char_or_word'] == "char":
+				print ''.join(sent)
+			else:
+				print ' '.join(sent)
+
+
+if __name__ == "__main__":
+	main()
---
+++ code/token_level_model/solver.py
@@ -0,0 +1,269 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.contrib import rnn
+from utilities import OutputSentence, TopN
+import utilities
+import utilities as utils
+import model
+	
+class Solver:
+
+	def __init__(self, buckets=None, mode='training'):
+		if mode=='training':
+			self.model_obj = model.RNNModel(buckets, mode=mode)
+		else:
+			self.model_obj = model.RNNModel(buckets_dict=None, mode=mode)
+
+	def getModel(self, config, buckets, mode='train', reuse=False ):
+
+		self.buckets = buckets 
+		self.preds = []
+		self.cost_list = []
+		self.optimizer_list = []
+		self.mask_list = []
+		self.token_input_sequences_placeholder_list = []
+		self.token_output_sequences_placeholder_list = []
+
+		optimizer_typ = "sgd" #"adam"
+		if "optimizer_typ" in config:
+			optimizer_typ = config['optimizer_typ']
+		self.optimizer_typ = optimizer_typ
+		learning_rate= 0.5 #0.001
+		print "optimizer_typ, learning_rate= ", optimizer_typ, learning_rate
+
+		if mode=='train':
+			for bucket_num, bucket_dct in self.buckets.items():
+				config['max_inp_seq_length'] = bucket_dct['max_input_seq_length']
+				print ""
+				print "------------------------------------------------------------------------------------------------------------------------------------------- "
+				pred = self.model_obj.getDecoderModel(config, is_training=True, mode='training', reuse=reuse, bucket_num=bucket_num)
+				self.preds.append(pred)
+				cost = self.model_obj.cost
+				self.cost_list.append(cost)
+				if self.optimizer_typ=="sgd":
+					optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+					train_op = optimizer
+				else: # adam
+					optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+					grads = tf.gradients(cost, tf.trainable_variables())
+					grads_and_vars = list(zip(grads, tf.trainable_variables()))
+					train_op = optimizer.apply_gradients(grads_and_vars=grads_and_vars)
+				self.optimizer_list.append(train_op)
+				reuse=True
+			self.token_input_sequences_placeholder_list  = self.model_obj.token_input_sequences_placeholder_list
+			self.token_output_sequences_placeholder_list = self.model_obj.token_output_sequences_placeholder_list
+			self.mask_list = self.model_obj.masker_list
+			self.weight_list = self.model_obj.weight_list
+			
+			# for predictions / sampler
+			bucket_num_for_preds = 0 # TO DO: creat separate input placeholder for this
+			config['keep_prob'] = 1.0 # keep 1.0 during test time
+			decoder_outputs_preds = self.model_obj.getDecoderModel(config, is_training=False, mode='training', reuse=True, bucket_num=bucket_num_for_preds)	
+			self.decoder_inputs_preds = self.token_input_sequences_placeholder_list[bucket_num_for_preds]
+			self.decoder_outputs_preds = decoder_outputs_preds
+		else:
+			config['keep_prob'] = 1.0 # keep 1.0 during test time
+			decoder_outputs_preds = self.model_obj.getDecoderModel(config, is_training=False, mode='inference', reuse=reuse)	
+			self.decoder_inputs_preds = self.model_obj.token_input_sequences_placeholder_inference
+			self.decoder_outputs_preds = decoder_outputs_preds
+			
+	def trainModel(self, config, train_feed_dict, val_feed_dct, reverse_vocab, do_init=True):
+
+		# Initializing the variables
+		if do_init:
+			init = tf.global_variables_initializer()
+			sess = tf.Session()
+			sess.run(init)
+			self.sess= sess
+
+		saver = tf.train.Saver()
+
+		print("============== \n Printing all trainainble variables")
+		for v in tf.trainable_variables():
+			print(v)
+		print("==================")
+
+
+		for bucket_num,bucket in enumerate(self.buckets): # move training_iters loop before this
+			input_sequences, output_sequences, train_loss_weights = train_feed_dict[bucket_num]
+			print "input_sequences[0] = ", input_sequences[0]
+			print "output_sequences[0] = ", output_sequences[0]
+			#cost = self.model_obj.cost
+
+			# if y is passed as (N, seq_length, 1): change it to (N,seq_length)
+			if len(output_sequences.shape)==3:
+				output_sequences=np.reshape(output_sequences, (output_sequences.shape[0], output_sequences.shape[1]))
+
+			#create temporary feed dictionary
+			token_input_sequences_placeholder = self.token_input_sequences_placeholder_list[bucket_num]
+			token_output_sequences_placeholder = self.token_output_sequences_placeholder_list[bucket_num]
+			loss_weights_placeholder = self.weight_list[bucket_num]
+			feed_dct={token_input_sequences_placeholder:input_sequences, token_output_sequences_placeholder:output_sequences, loss_weights_placeholder:train_loss_weights }
+
+			pred = self.preds[bucket_num]
+			masker = self.mask_list[bucket_num]
+			cost = self.cost_list[bucket_num]
+
+			train_op = self.optimizer_list[bucket_num]
+			
+			sess = self.sess
+
+			batch_size=config['batch_size']
+			training_iters=50
+			display_step=1
+			sample_step=2
+			save_step = 1
+			n = feed_dct[token_input_sequences_placeholder].shape[0]
+
+			# Launch the graph
+			step = 1
+			#preds = np.array( sess.run(self.pred, feed_dict= feed_dct) )
+			#print preds
+			#with tf.Session() as sess:
+			while step < training_iters:
+				print "STEP = ",step
+				#num_of_batches =  n/batch_size #(n+batch_size-1)/batch_size
+				num_of_batches =  (n+batch_size-1)/batch_size
+				for j in range(num_of_batches):
+					if j%100==0:
+						print "batch= ",j
+					feed_dict_cur = {}
+					for k,v in feed_dct.items():
+						feed_dict_cur[k] = v[j*batch_size:min(n,(j+1)*batch_size)]
+						#print feed_dict_cur[k].shape
+					cur_out = feed_dict_cur[token_output_sequences_placeholder]
+					x,y = np.nonzero(cur_out)
+					mask = np.zeros(cur_out.shape, dtype=np.float)
+					mask[x,y]=1
+					feed_dict_cur[masker] = mask
+					sess.run(train_op, feed_dict=feed_dict_cur )
+
+				if step % display_step == 0:
+	  				val_x,val_y = val_feed_dct
+					#self.getLoss(config, val_x, val_y, token_input_sequences_placeholder, token_output_sequences_placeholder, masker, loss_weights_placeholder ,cost, sess)
+	  				self.solveAll(config, val_x, val_y, reverse_vocab, sess)
+
+				if step % sample_step == 0:
+  					self.runInference( config, input_sequences[:batch_size], output_sequences[:batch_size], reverse_vocab, sess )
+					'''pred_cur = np.array( sess.run(pred, feed_dict= feed_dict_cur) )
+					print pred_cur.shape
+					print pred_cur[0].shape
+					print np.sum(pred_cur[0],axis=1)
+					'''
+				if step%save_step==0:
+					save_path = saver.save(sess, "./tmp/tf/model_sgd_WordToWord"+str(step)+".ckpt")
+	  				print "Model saved in file: ",save_path
+
+				step += 1
+
+		self.saver = saver
+
+
+	###################################################################################
+
+	def runInference(self, config, input_sequences, output_sequences, reverse_vocab, sess=None, print_all=True): # Probabilties
+		if print_all:
+			print " Inference STEP ...... ============================================================"
+		if sess==None:
+			print "sess is None.. LOAD?ING SAVED MODEL"
+	  		sess = tf.Session()
+	  		saver = tf.train.Saver()
+	  		saved_model_path = config['saved_model_path']
+	  		print "Loading saved model from : ",saved_model_path
+	  		saver.restore(sess, saved_model_path)
+		model_obj = self.model_obj
+		end_index = 2 # TO_DO load this from config
+		feed_dct={self.decoder_inputs_preds:input_sequences}
+		batch_size = config['batch_size'] #x_test.shape[0]
+		preds =  self.decoder_outputs_preds
+		preds = np.array( sess.run(preds, feed_dict= feed_dct) ) # timesteps, N, vocab_size
+		timesteps, N, vocab_size = preds.shape
+		assert N==batch_size
+		probs = np.zeros(batch_size)
+		for i in range(batch_size):
+			t=0 
+			while t<timesteps and output_sequences[i][t]!=0:
+				next_word = output_sequences[i][t]
+				next_word_prob = preds[t][i][next_word]
+				probs[i] += np.log(next_word_prob)
+				t+=1
+		if print_all:
+			print "log likelihoods are as follows : "
+			print probs
+		return probs
+
+	###################################################################################
+
+	def solveAll(self, config, input_sequences, output_sequences, reverse_vocab, sess=None, dump_seq_prob=False, dump_seq_prob_path=None): # Probabilities
+		print " SolveAll ...... ============================================================"
+		if sess==None:
+			print "sess is None.. LOAD?ING SAVED MODEL"
+	  		sess = tf.Session()
+	  		saver = tf.train.Saver()
+	  		saved_model_path = config['saved_model_path']
+	  		print "Loading saved model from : ",saved_model_path
+	  		saver.restore(sess, saved_model_path)
+		batch_size = config['batch_size']
+		num_batches = ( len(input_sequences) + batch_size - 1)/ batch_size 
+		probs = []
+		for i in range(num_batches):
+			#print "i= ",i
+			cur_input_sequences = input_sequences[i*batch_size:(i+1)*batch_size]
+			cur_output_sequences = output_sequences[i*batch_size:(i+1)*batch_size]
+			lim = len(cur_input_sequences)
+			if len(cur_input_sequences)<batch_size:
+				gap = batch_size - len(cur_input_sequences)
+				for j in range(gap):
+					cur_output_sequences = np.vstack( (cur_output_sequences, cur_output_sequences[0]) )
+					cur_input_sequences = np.vstack( (cur_input_sequences, cur_input_sequences[0]) )
+			cur_probs = self.runInference(config, cur_input_sequences, cur_output_sequences, reverse_vocab, sess=sess, print_all=False)
+			probs.extend( cur_probs[:lim] )
+		probs = np.array(probs)
+		print "PREPLEXITY = ", utils.getPerplexityFromSumProbs(probs, output_sequences)
+		if dump_seq_prob:
+			prob_vals = utils.getSequenceProbs(probs, output_sequences)
+			print "Dumping values to path= ",dump_seq_prob_path
+			print "prob_vals.shape = ",prob_vals.shape
+			fw = open(dump_seq_prob_path,"w")
+			for gt_sequence,seq_prob in zip(output_sequences, prob_vals):
+				#print "gt_sequence = ",gt_sequence
+				#print "seq__prob = ",seq_prob
+				gt_str_seq = ""
+				for j in gt_sequence:
+					if j==0:
+						break
+					gt_str_seq = gt_str_seq + " " + reverse_vocab[j]
+				fw.write( gt_str_seq + "\t" + str(seq_prob) + "\n" )
+			fw.close()
+
+	###################################################################################
+
+	def getLoss(self, config, input_sequences, output_sequences, loss_weights, inp_placeholder, out_placeholder, mask_placeholder, loss_weights_placeholder ,loss_variable, sess): # Probabilities
+		print " getLoss ...... ============================================================"
+		batch_size = config['batch_size']
+		num_batches = ( len(input_sequences) + batch_size - 1)/ batch_size 
+		loss = []
+		for i in range(num_batches):
+			#print "i= ",i
+			cur_input_sequences = input_sequences[i*batch_size:(i+1)*batch_size]
+			cur_output_sequences = output_sequences[i*batch_size:(i+1)*batch_size]
+			cur_loss_weights = loss_weights[i*batch_size:(i+1)*batch_size]
+			lim = len(cur_input_sequences)
+			if len(cur_input_sequences)<batch_size:
+				gap = batch_size - len(encoder_inputs_cur)
+				for j in range(gap):
+					cur_output_sequences = np.vstack( (cur_output_sequences, cur_output_sequences[0]) )
+					cur_input_sequences = np.vstack( (cur_input_sequences, cur_input_sequences[0]) )
+					cur_loss_weights = np.vstack( (cur_loss_weights, 0) )
+			feed_dct = {inp_placeholder:cur_input_sequences, out_placeholder:cur_output_sequences, loss_weights_placeholder:cur_loss_weights}
+			mask = np.zeros(cur_output_sequences.shape, dtype=np.float)
+			x,y = np.nonzero(cur_output_sequences)
+			mask[x,y]=1
+			feed_dct[mask_placeholder]=mask
+			cur_loss = sess.run(loss_variable, feed_dct)
+			loss.append( cur_loss )
+		loss = np.array(loss)
+		print "LOSS = ", np.mean(loss)
+
+
+########################################################################################
